a9ec1153bb759321b0f277e63e64a86e47a6d13e
Postnov Daniil <dan.postnov@gmail.com>
2019-02-16 09:18:31 +0300

    add header, search, list components

--- src/index.js
+++ src/index.js
@@ -1,1 +1,32 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const AppHeader = () => {
+    return <h1>My todo list</h1>;
+}
+
+const InputSearch = () => {
+    return <input  placeholder="Search"/>;
+}
+
+const TodoList = () => {
+    return (
+        <ul>
+            <li>Learn React</li>
+            <li>Build awesome app</li>
+        </ul>
+    );
+}
+
+
+const App = () => {
+    return (
+        <div>
+            <AppHeader />
+            <InputSearch />
+            <TodoList />
+        </div>
+    )
+}
+
+ReactDOM.render(<App/>, document.querySelector('#root'));
