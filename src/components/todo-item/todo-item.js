e39e4d908c2a8c338a28966c0dc2a9177ca0d603
Postnov Daniil <dan.postnov@gmail.com>
2019-02-16 12:42:15 +0300

    rewrite function on classes declaration

--- src/components/todo-item/todo-item.js
+++ src/components/todo-item/todo-item.js
@@ -1,30 +1,33 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './todo-item.css';
 
-const TodoItem = ({label, important = false}) => {
-    const style = {
-        color: important ? 'steelblue' : null,
-        fontWeight: important ? 'bold' : 'normal'
-    }
-    return (
-        <span className="todo-item">
-        <span
-          className="todo-item-label"
-          style={style}>
-          {label}
-        </span>
+export default class TodoItem extends Component {
 
-        <button type="button"
-                className="btn btn-outline-success btn-sm float-right">
-          <i className="fa fa-exclamation" />
-        </button>
+	render() {
+		let {label, important = false} = this.props;
 
-        <button type="button"
-                className="btn btn-outline-danger btn-sm float-right">
-          <i className="fa fa-trash-o" />
-        </button>
-      </span>
-    );
-}
+		const style = {
+			color: important ? 'steelblue' : null,
+			fontWeight: important ? 'bold' : 'normal'
+		}
+		return (
+			<span className="todo-item">
+			<span
+			  className="todo-item-label"
+			  style={style}>
+			  {label}
+			</span>
 
-export default TodoItem;
+			<button type="button"
+					className="btn btn-outline-success btn-sm float-right">
+			  <i className="fa fa-exclamation" />
+			</button>
+
+			<button type="button"
+					className="btn btn-outline-danger btn-sm float-right">
+			  <i className="fa fa-trash-o" />
+			</button>
+		  </span>
+		);
+	}
+};
