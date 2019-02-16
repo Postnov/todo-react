7aa8d5e76a9d90130637f381fc179f8a7faf517d
Postnov Daniil <dan.postnov@gmail.com>
2019-02-16 12:09:59 +0300

    add styles in item, add buttons for interaction

--- src/components/todo-item.js
+++ src/components/todo-item.js
@@ -1,10 +1,30 @@
 import React from 'react';
+import './todo-item.css';
 
 const TodoItem = ({label, important = false}) => {
     const style = {
-        color: important ? 'tomato' : null
+        color: important ? 'steelblue' : null,
+        fontWeight: important ? 'bold' : 'normal'
     }
-    return <span style={style}>{label}</span>;
+    return (
+        <span className="todo-item">
+        <span
+          className="todo-item-label"
+          style={style}>
+          {label}
+        </span>
+
+        <button type="button"
+                className="btn btn-outline-success btn-sm float-right">
+          <i className="fa fa-exclamation" />
+        </button>
+
+        <button type="button"
+                className="btn btn-outline-danger btn-sm float-right">
+          <i className="fa fa-trash-o" />
+        </button>
+      </span>
+    );
 }
 
 export default TodoItem;
