2ff4d9f70670b7158f8aa58aba1d511867416a7f
Postnov Daniil <dan.postnov@gmail.com>
2019-02-17 14:01:57 +0300

    add addition text in list

--- src/components/add-todo/add-todo.js
+++ src/components/add-todo/add-todo.js
@@ -5,24 +5,34 @@ import './add-todo.css'
 export default class Add extends Component{
 
     state = {
-        text: 'I love React'
+        text: ''
     }
 
+    onInputChange = (e) => {
+        this.setState({ text: e.target.value });
+    }
 
+    onSubmit = (e) => {
+        e.preventDefault();
+        this.props.onAdded(this.state.text);
+        this.setState({text: ''});
+    }
 
     render() {
-        let {onAdded} = this.props;
-
+        let {text} = this.state;
         return (
-            <form className="add-todo d-flex">
+            <form
+                className="add-todo d-flex"
+                onSubmit={this.onSubmit}>
                 <input
                     type="text"
                     className="form-control"
+                    value={text}
                     onChange={this.onInputChange}
                     placeholder="Enter you task in here"/>
                 <button
                     className="btn btn-outline-secondary"
-                    onClick={() => onAdded(this.state.text)}>
+                    >
                 Add item</button>
             </form>
         )
